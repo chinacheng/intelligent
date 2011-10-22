da2f347154c3856a02ab7a22960d7f230cbc0ca5
chinacheng <chinachengliwen@gmail.com>
2011-10-22 21:45:48 +0800

    add the user model test,successed to add a new user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,21 @@ require "uuidtools"
 
 class User < ActiveRecord::Base
 
-  validates_presence_of :name, :email, :gender, :password, :login, :salt
-  validates_uniqueness_of :login
+  validates_presence_of :name, :email, :gender, :password,:password_confirmation, :login, :salt
+  validates_confirmation_of :password
+  validates_uniqueness_of :login,:email
+  validates_length_of :login,:within=>4..60
+  validates_length_of :password,:within=>4..60
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  attr_protected :id,:salt
+  attr_accessor :password,:password_confirmation
 
   GENDER_MALE = "male"
   GENDER_FEMALE = "female"
 
-  before_create :set_salt_and_password
-  def set_salt_and_password
+  def password=(pass)
+    @password = pass
     self.salt = User.random_string(10) if !self.salt?
     self.hashed_password = User.encrypt(@password, self.salt)
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  # successed create a user model 
+  test "create_user_success_test" do
+    assert_difference "User.count",1 do 
+      user = User.new(:name=>"test_name",:login=>"test_login",:email=>"user@test.com",
+                      :password=>"123456",:password_confirmation=>"123456",:gender=>User::GENDER_MALE)
+      
+      assert_equal true,user.valid?
+      assert_equal true,user.save
+   
+      assert_equal user.hashed_password,User.encrypt("123456", user.salt)
+    end
   end
+
 end
