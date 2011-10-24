95bd3f93a54669b16021018530bb7abfd30da1e4
haizhenglan <haizhenglang@yahoo.com.cn>
2011-10-24 20:30:51 +0800

    update Menu mode

--- db/migrate/20111022133312_create_roles.rb
+++ db/migrate/20111022133312_create_roles.rb
@@ -1,7 +1,7 @@
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
-      t.string :name, :null => false,:limit => 32
+			t.string :name, :null => false,:limit => 32
       t.string :remark, :limit => 255
       t.timestamps
     end
