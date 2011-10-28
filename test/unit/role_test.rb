12b4cabcabec7effbbaad7da0eb3265a2e776525
haizhenglan <haizhenglang@yahoo.com.cn>
2011-10-28 19:50:23 +0800

    Test file format

--- test/unit/role_test.rb
+++ test/unit/role_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-	test "create_role_success_test" do
-		assert_difference "Role.count",1 do
-			role = Role.new(:name => "123456")
-
-			assert_equal true,role.valid?
-			assert_equal true,role.save
-		end
-	end
+  test "create_role_success_test" do
+    assert_difference "Role.count",1 do
+      role = Role.new(:name => "123456")
+      assert_equal true,role.valid?
+      assert_equal true,role.save
+    end
+  end
 end
