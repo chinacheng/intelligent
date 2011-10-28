6503102c3741a48e916c59de117b2ea5d32b5e56
chinacheng <chinachengliwen@gmail.com>
2011-10-28 21:34:16 +0800

    modify the format of tests files

--- test/unit/menu_test.rb
+++ test/unit/menu_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class MenuTest < ActiveSupport::TestCase
-	test "create_role_success_test" do
-		assert_difference "Menu.count",1 do
-			menu = Menu.new(:name => "admins",:app_id => "admin",:sequence => 1,:tip => "test of menus",
-								:image => "images/menus/1.gif")
+  test "create_role_success_test" do
+    assert_difference "Menu.count",1 do
+      menu = Menu.new(:name => "admins",:app_id => "admin",:sequence => 1,:tip => "test of menus",
+                      :image => "images/menus/1.gif")
 
-			assert_equal true,menu.valid?
-			assert_equal true,menu.save		
-		end
-	end
+      assert_equal true,menu.valid?
+      assert_equal true,menu.save		
+    end
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,10 +7,10 @@ class UserTest < ActiveSupport::TestCase
     assert_difference "User.count",1 do 
       user = User.new(:name=>"test_name",:login=>"test_login",:email=>"user@test.com",
                       :password=>"123456",:password_confirmation=>"123456",:gender=>User::GENDER_MALE)
-      
+
       assert_equal true,user.valid?
       assert_equal true,user.save
-   
+
       assert_equal user.hashed_password,User.encrypt("123456", user.salt)
     end
   end
