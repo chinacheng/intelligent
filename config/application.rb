764f7ba6b8dde60fa265e3587c009c847f4e160a
chinacheng <chinachengliwen@gmail.com>
2012-03-07 22:49:49 +0800

    time zone setting

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module Intelligent
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Beijing'
-    config.active_record.default_timezone = :Beijing
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
