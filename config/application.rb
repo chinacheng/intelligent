92eb31f9f737e2af5f79430af5756bbefd576430
chinacheng <chinachengliwen@gmail.com>
2012-03-16 22:27:14 +0800

    use asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,9 @@ module Intelligent
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    # Enable the asset pipeline                                                                          
+    config.assets.enabled = true
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
